5cb17720f7d9c4798b917c7d9c02f1202c6c9c5e
Matt Criswell <llewsircttam@gmail.com>
2023-03-05 15:24:09 -0600

    day 7 part 1

--- day07/files.py
+++ day07/files.py
@@ -1,9 +1,10 @@
+''' takes command line history as output and parses into a filesystem structure '''
 from scratch import File, Directory
 
 #INPUT = 'test.txt'
 INPUT = 'input.txt'
 
-with open(INPUT, 'rt') as infile:
+with open(INPUT, 'rt', encoding='UTF-8') as infile:
     contents = infile.readlines()
 
 cli_output = [line.split() for line in [entry.strip() for entry in contents]]
@@ -11,6 +12,7 @@ cli_output = [line.split() for line in [entry.strip() for entry in contents]]
 work_dir = []
 journal = {}
 for line in cli_output:
+    # check if command
     if line[0] == '$':
         if line[1] == 'cd':
             if line[2] == '/':
@@ -20,22 +22,24 @@ for line in cli_output:
                 work_dir = work_dir[:-2]
             else:
                 test_key = ''.join(work_dir) + line[2] + '/'
-                if test_key not in journal.keys():
+                if test_key not in journal:
                     journal[test_key] = Directory(test_key)
                     journal[''.join(work_dir)].add(journal[test_key])
                 work_dir.append(line[2])
                 work_dir.append('/')
+    # else check if line is a dir
     elif line[0] == 'dir':
         test_key = ''.join(work_dir) + line[1] + '/'
-        if test_key not in journal.keys():
+        if test_key not in journal:
             journal[test_key] = Directory(test_key)
             journal[''.join(work_dir)].add(journal[test_key])
+    # else is must be a file
     else:
         journal[''.join(work_dir)].add(File(line[1], int(line[0])))
 
 journal['/'].operation()
-dirs_total = 0
+DIRS_TOTAL = 0
 for i in journal:
     if journal[i].file_size <= 100000:
-        dirs_total += journal[i].file_size
-print(dirs_total)
+        DIRS_TOTAL += journal[i].file_size
+print(DIRS_TOTAL)
--- day07/scratch.py
+++ day07/scratch.py
@@ -1,8 +1,10 @@
-class File:
+''' simple composite pattern to represent a file system '''
+from __future__ import annotations
+from abc import ABC, abstractmethod
 
-    def __init__(self, name, file_size=0):
-        self.name = name
-        self.file_size = file_size
+
+class Component(ABC):
+    ''' base component class '''
 
     @property
     def parent(self):
@@ -17,53 +19,54 @@ class File:
         return self._file_size
 
     @file_size.setter
-    def file_size(self, value):
+    def file_size(self, value: int):
         if not isinstance(value, int):
             raise TypeError('Expected int')
         self._file_size = value
 
-    def is_composite(self):
+    def add(self, component: Component) -> None:
+        pass
+
+    def remove(self, component: Component) -> None:
+        pass
+
+    def is_composite(self) -> bool:
         return False
+
+    @abstractmethod
+    def operation(self) -> int:
+        pass
+
+
+class File(Component):
+    ''' "leaf" class '''
+
+    def __init__(self, name: str, file_size: int = 0):
+        self.name = name
+        self.file_size = file_size
 
     def operation(self) -> int:
         return self.file_size
 
 
 class Directory:
+    ''' "composite" class '''
 
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.name = name
         self.children = []
         self.file_size = 0
         self.parent = self
 
-    @property
-    def parent(self):
-        return self._parent
-
-    @parent.setter
-    def parent(self, parent):
-        self._parent = parent
-
-    @property
-    def file_size(self):
-        return self._file_size
-
-    @file_size.setter
-    def file_size(self, value):
-        if not isinstance(value, int):
-            raise TypeError('Expected int')
-        self._file_size = value
-
-    def add(self, component):
+    def add(self, component: Component):
         self.children.append(component)
         component.parent = self
 
-    def remove(self, component):
+    def remove(self, component: Component):
         self.children.remove(component)
         component.parent = None
 
-    def is_composite(self):
+    def is_composite(self) -> bool:
         return True
 
     def operation(self) -> int:
@@ -73,17 +76,6 @@ class Directory:
                 item.operation()
             self.file_size += item.file_size
         return self.file_size
-
-    def special_operation(self):
-        output = []
-        for item in self.children:
-            if isinstance(item, Directory):
-                print((item.name, item.file_size))
-                item.special_operation()
-                #item.special_operation()
-                #if item.file_size >= 100000:
-                #output.append((item.file_size,item.special_operation()))
-        #return output
 
 
 if __name__ == '__main__':
